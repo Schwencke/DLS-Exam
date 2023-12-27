cd5afd3d08f4409e015d46fee73eeef7fd28e441
Thomas Schwencke Overgaard <thomas@tech-logic.dk>
2023-12-27 12:20:29 +0100

    init

---
+++ src/main/java/com/schwencke/dlsexam/DlsExamApplication.java
@@ -0,0 +1,13 @@
+package com.schwencke.dlsexam;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DlsExamApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(DlsExamApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/schwencke/dlsexam/DlsExamApplicationTests.java
@@ -0,0 +1,13 @@
+package com.schwencke.dlsexam;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DlsExamApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
