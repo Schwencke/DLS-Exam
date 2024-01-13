79f56acfcb4fd9d1598d7f926d4f3c8cc1dadc41
Thomas Schwencke Overgaard <thomas@tech-logic.dk>
2024-01-13 15:26:09 +0100

    test med side

--- src/main/java/com/schwencke/dlsexam/DlsExamApplication.java
+++ src/main/java/com/schwencke/dlsexam/DlsExamApplication.java
@@ -13,8 +13,5 @@ public class DlsExamApplication {
     public static void main(String[] args) {
         SpringApplication.run(DlsExamApplication.class, args);
     }
-    @GetMapping("/hello")
-    public String hello(@RequestParam(value = "name", defaultValue = "World") String name) {
-        return String.format("Hello %s!", name);
-    }
+
 }
